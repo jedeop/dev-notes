2ab8b930b61e359806462895c48d35879b152a0c
jedeop <jedeop@jedeop.dev>
2023-07-14 21:36:32 +0900

    ✨ feat(frontend): static render category page

--- frontend/pages/c/[category].tsx
+++ frontend/pages/c/[category].tsx
@@ -1,16 +1,28 @@
 import { useRouter } from 'next/router'
 import MainView from '../../components/MainView'
 import { DEFAULT_TITLE, Title } from '../../common/title';
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next';
 
-export const getServerSideProps: GetServerSideProps<{title: Title}> = async ({ params }) => {
-  const res = await fetch(`https://note.jedeop.dev/api/category/${params?.category || "default"}`)
+export const getStaticProps: GetStaticProps<{title: Title}> = async ({ params }) => {
+  const res = await fetch(`${process.env.API_BASE_URI}/api/category/${params?.category || "default"}`)
   const data: Title = await res.json()
  
   return { props: { title: data } }
 }
 
-export default function CategoryView({ title }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export const getStaticPaths: GetStaticPaths = async () => {
+  const res = await fetch(`${process.env.API_BASE_URI}/api/categories`)
+  const data: string[] = await res.json()
+  const paths = data.map(category => ({
+    params: { category },
+  }))
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export default function CategoryView({ title }: InferGetStaticPropsType<typeof getStaticProps>) {
   const router = useRouter();
   const category = router.query.category as string;
   return (
